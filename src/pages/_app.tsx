945908d7a70705801ff91f7938d3c4dc00d55f53
임병채 <ithing108@gmail.com>
2021-08-29 17:12:12 +0900

    chore: viewport 설정 추가

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -10,8 +10,16 @@ function DoSports({ Component }: AppProps): ReactFragment {
   return (
     <>
       <Head>
-        <meta charSet="utf-8" />
         <title>DoSports!</title>
+        <meta charSet="utf-8" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1, user-scalable=no"
+        />
+        <meta name="og:title" content="DoSports" />
+        <meta name="og:url" content="" />
+        <meta name="og:image" content="" />
+        <meta name="og:description" content="DoSports에서 다양한 스포츠 정보를 만나 보세요" />
       </Head>
       <RecoilRoot>
         <Component />
