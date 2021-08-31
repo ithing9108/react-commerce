b6abd285c5cc86ab1a4cd5cc943f483fe287b6b1
임병채 <ithing108@gmail.com>
2021-09-01 00:35:54 +0900

    chore: nextjs 버전 10으로 변경 및 inline-react-svg 추가

--- next.config.js
+++ next.config.js
@@ -1,23 +1,48 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
+const withPlugins = require('next-compose-plugins');
 const withSass = require('@zeit/next-sass');
 const withLess = require('@zeit/next-less');
 const withCSS = require('@zeit/next-css');
+const images = require('next-images');
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+});
 
 if (typeof require !== 'undefined') {
   require.extensions['.less'] = () => ({});
 }
 
-module.exports = withCSS({
-  cssModules: true,
-  cssLoaderOptions: {
-    importLoaders: 1,
-    localIdentName: '[local]___[hash:base64:5]',
+module.exports = withPlugins(
+  [
+    [
+      withCSS({
+        cssModules: true,
+        cssLoaderOptions: {
+          importLoaders: 1,
+          localIdentName: '[local]___[hash:base64:5]',
+        },
+        ...withLess(
+          withSass({
+            lessLoaderOptions: {
+              javascriptEnabled: true,
+            },
+          }),
+        ),
+      }),
+    ],
+    withBundleAnalyzer,
+    images,
+  ],
+  {
+    compress: true,
+    webpack(config, { webpack }) {
+      const prod = process.env.NODE_ENV === 'production';
+      return {
+        ...config,
+        mode: prod ? 'production' : 'development',
+        devtool: prod ? 'hidden-source-map' : 'eval',
+        plugins: [...config.plugins, new webpack.ContextReplacementPlugin(/moment[/\\]locale$/, /^\.\/ko$/)],
+      };
+    },
   },
-  ...withLess(
-    withSass({
-      lessLoaderOptions: {
-        javascriptEnabled: true,
-      },
-    }),
-  ),
-});
+);
